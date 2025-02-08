da3c891dd94f5a454106fb698035bb4c6ccbbd27
granPiero <fischer.pj^gmail.com>
2025-02-08 20:26:19 +0000

    2504

--- posts/mapStorage/map_storage.js
+++ posts/mapStorage/map_storage.js
@@ -1,3 +1,4 @@
+// 1.1.2 lastEntry
 // 1.1.1 refPayload
 // 1.1.0 refEntries
 // 1.0.8 XfetchDataInParallel
@@ -67,28 +68,33 @@ class MapStor {
 // GETTER methods
 
   // Getter for source
+  get keysArray() {
+    return Array.from(this.#refPayload.keys());
+  }
+  
+  // Getter for source
   get refSource() {
     return this.#refSource;
   }
   
-  // Getter for refEntries ARRAY in full
-  get refEntries() {
-    return this.#refEntries;
+  // Getter for the map as ARRAY in full
+  get asEntries() {
+    return Array.from(this.#refPayload.entries()).reverse();
   }
   
   // Getter for refPayload
   get refPayload() {
-    return   this.#refPayload;
+    return  this.#refPayload;
   }
   
   // Getter for lastEntry 
   get lastEntry() {
-    return this.#refPayload.slice(-1);
+    return Array.from(this.#refPayload.entries()).slice(-1);
   }
 
   // Getter for a in refMap's random entry as MAP
   get randomEntry() {
-    const myload = this.#refPayload;
+    const myload = Array.from(this.#refPayload.entries());
     const randomIndex = Math.floor(Math.random() * myload.length);
     return myload.slice(randomIndex, randomIndex + 1);
   }
@@ -106,29 +112,25 @@ class MapStor {
     localStorage.setItem('refEntries', JSON.stringify(this.#refEntries));
   }
   
-  asEntry(refid) {
-    const entry = [];
-//      this.#refEntries = JSON.parse(refidArray).map(item => [item.refid, {Xurl: `${this.#Xurlbase}?param=${item.refid}`, ts: item.ts, category: 'biblical'}]);
-    entry;
+  addEntry(refid= 'ps116:1') {
+    const new_entries = this.#refEntries;
+
+    // create a value timestamped
+    const objvalue = {Xurl: `${this.#Xurlbase}?param=${refid}`};
+      objvalue['ts'] = new Date().toISOString();
+      objvalue['category'] = 'biblical';
+    
+    new_entries.push([refid, objvalue]);
+    this.#refEntries = new_entries;
   }
   
 // =============================================================================
 // MAP methods
 
-  // Method to add an entry
-  // add an element to Array
-    addElement(refid) {
-      const new_array = this.#refEntries;
-//      const item = this.timestamp_(refid);
-//      new_array.push(item);
-//      this.#cachedValue = new_array;
-//      this.write_storage();
-    }
-
   // Method to remove the last entry
 
-  mapHTML(){
-    const result = this.refPayload;
+  mapHTML(refMap = this.asEntries){
+    const result = refMap;
     const anchor = document.getElementById('mainAnchor');
     const ul = document.createElement('ul');
     // Clear the existing UL content
@@ -137,7 +139,10 @@ class MapStor {
     
     // Append all the updated LI elements to the UL in one step
       result.forEach(([key,value]) => {
+        console.log(key);
+        console.log(value);
         let content_result = value.content.replace(/^\d+/, '');
+//        let content_result = value.content;
         const li = document.createElement('li');
           li.textContent = key + " " +content_result;
           ul.appendChild(li)
@@ -161,7 +166,7 @@ class MapStor {
 
     // Wait for all fetch promises to resolve
     const results  = await Promise.all(fetchPromises);
-    this.#refPayload = results;
+    this.#refPayload = new Map(results);
 
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -175,14 +180,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // Create an instance of MapStor, which will initialize based on the conditions provided
   const mapStor = new MapStor();
   console.log(mapStor.refSource); // Log the refSource
+  mapStor.addEntry('ps118:1'); // 
 
   mapStor.XfetchParallel()
   .then(() => {
-    console.log(mapStor.refEntries);
-    console.log(mapStor.refPayload);
+    const keys = [['refKeys',{content: JSON.stringify(mapStor.keysArray)}]];
+    console.log(mapStor.keysArray);
+//    console.log(mapStor.asEntries);
+//    console.log(mapStor.refPayload);
     console.log(mapStor.lastEntry);
-    console.log(mapStor.randomEntry);
-    mapStor.mapHTML();
+    console.log(JSON.stringify(mapStor.randomEntry));
+    mapStor.mapHTML(this.asEntries);
+//    mapStor.mapHTML(keys);
   });
   
 });
